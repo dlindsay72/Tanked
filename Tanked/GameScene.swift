3eca38b95bb2a3b8cab258d453518cdaf1fe2fff
Dan Lindsay <dlindsay72@hotmail.com>
2016-11-02 14:20:17 -0400

    Can now run the project with the map appearing and able to move it around

--- Tanked/GameScene.swift
+++ Tanked/GameScene.swift
@@ -7,20 +7,42 @@
 //
 
 import SpriteKit
-import GameplayKit
+
 
 class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    var lastTouch = CGPoint.zero
+    var originalTouch = CGPoint.zero
+    var cameraNode: SKCameraNode!
+    
     override func didMove(to view: SKView) {
         
-       
+       cameraNode = camera!
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        guard let touch = touches.first else { return }
+        
+        lastTouch = touch.location(in: self.view)
+        
+        originalTouch = lastTouch
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else { return }
+        
+        let touchLocation = touch.location(in: self.view)
+        let newX = cameraNode.position.x + (lastTouch.x - touchLocation.x)
+        let newY = cameraNode.position.y + (touchLocation.y - lastTouch.y)
+        
+        cameraNode.position = CGPoint(x: newX, y: newY)
+        
+        lastTouch = touchLocation
     }
     
    
